66bc4ada840aa2d27fdefd6a44047bc49371d5bc
Баранов Максим Евгеньевич <maxim.baranov@russianpost.ru>
2021-07-10 22:33:48 +0300

    Initial commit

---
+++ oauth2-client-server-server/oauth2-client-server-server-api/src/main/java/com/example/oauth2clientserverserverapi/Oauth2ClientServerServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2clientserverserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientServerServerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientServerServerApiApplication.class, args);
+    }
+
+}
---
+++ oauth2-client-server-server/oauth2-client-server-server-api/src/test/java/com/example/oauth2clientserverserverapi/Oauth2ClientServerServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2clientserverserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientServerServerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
